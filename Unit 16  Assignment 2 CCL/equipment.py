a4c39b1a07012111c650f5c4d8ea0bb2c9980848
SahibS-U <166859815+SahibS-U@users.noreply.github.com>
2024-10-22 15:55:36 +0100

    Delete Unit 16  Assignment 2 CCL directory

--- Unit 16  Assignment 2 CCL/equipment.py
+++
@@ -1,6 +0,0 @@
-class Equipment:
-    def __init__(self, name, type, brand, description):
-        self.name = name
-        self.type = type
-        self.brand = brand
-        self.description = description
